2f34c6cc7668ab1d13fb6709bbc0bd6d5cbdbd5c
tranphuc7686 <tranphuc7686@gmail.com>
2018-01-08 16:00:44 +0700

    First Commit

---
+++ app/src/main/java/com/example/admin/trainningandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.admin.trainningandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn9,btn8,btn7,btn6,btn5,btn4,btn3,btn2,btn1,btnCong,btnTru,btnNhan,btnChia,btnAc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+    }
+}
